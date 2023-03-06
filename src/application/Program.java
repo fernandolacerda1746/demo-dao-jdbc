b735118d84bbf534ef8012832fa0f42dbfc942ff
fernandolacerda1746 <stingeralfa@gmail.com>
2023-03-06 11:48:46 -0300

    findAll Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -27,6 +27,15 @@ public class Program {
 			System.out.println(obj);
 			
 		}
+		
+		System.out.println("\n=== TEST 03: Seller 'findAll' ===");
+		list = sellerDao.findAll();
+		
+		for(Seller obj : list) {
+			
+			System.out.println(obj);
+			
+		}
 
 	}
 
